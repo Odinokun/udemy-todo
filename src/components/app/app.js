c60bd900e75f0bd389899cd054d6084eab369e70
odinokun <odinokun@gmail.com>
2019-01-24 12:36:53 +0200

    add addTask function

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,35 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './app.css';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
 
-import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-    const todoData = [
-        { label: 'Drink Coffee', important: false, id: 1 },
-        { label: 'Make Awesome App', important: true, id: 2 },
-        { label: 'Create portfolio', important: true, id: 3 },
-        { label: 'Find cool job', important: true, id: 4 },
-        { label: 'Go to fishing', important: false, id: 5 }
-    ];
+    maxId = 100;
 
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={2} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
+    state = {
+        todoData: [
+            { label: 'Drink Coffee', important: false, id: 1 },
+            { label: 'Make Awesome App', important: true, id: 2 },
+            { label: 'Create portfolio', important: true, id: 3 },
+            { label: 'Find cool job', important: true, id: 4 },
+            { label: 'Go to fishing', important: false, id: 5 }
+        ]
+    };
+
+    deleteItem = (id) => {
+        this.setState(({ todoData }) => {
+            //ищем индекс элемента у которого id точно такой как мы получили
+            const idx = todoData.findIndex((el) => el.id === id);
+
+            const newArray = [
+                //находим кусок массива ДО удаляемого элемента
+                ...todoData.slice(0, idx),
+                //находим кусок массива ПОСЛЕ удаляемого элемента
+                ...todoData.slice(idx + 1)
+            ];
+            //возвращаем измененный массив
+            return {
+                todoData: newArray
+            };
+        });
+    };
+
+    addItem = (text) => {
+        // генерируем id и элемент
+        const newItem = {
+            label: text,
+            important: false,
+            id: this.maxId++
+        };
+
+        // добавляем элемент в массив
+        this.setState(({ todoData }) => {
+            const newArr = [
+                //берем старый массив + новый элемент
+                ...todoData,
+                newItem
+            ];
+
+            return {
+                todoData: newArr
+            }
+        });
+    };
+
+    render() {
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={2} done={3} />
+                <div className="top-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+
+                <TodoList
+                    todos={this.state.todoData}
+                    onDeleted={ this.deleteItem }
+                    />
+
+                <ItemAddForm
+                    addItem={ this.addItem } />
             </div>
+        );
+    }
 
-            <TodoList
-                todos={todoData}
-                 onDeleted={ (id) => console.log('del', id) } />
-        </div>
-    );
 };
-
-export default App;
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
