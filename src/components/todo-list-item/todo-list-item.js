bae1753852f6a63a100890640046e2c928b8fd6f
odinokun <odinokun@gmail.com>
2019-01-31 11:49:48 +0200

    did search functional

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,63 +1,65 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            important: false
-        };
+    this.state = {
+      important: false
+    };
 
-        // при клике на кнопку добавляем/убираем к классу класс 'important'
-        // выделяем "дело" жирным шрифтом
-        this.onMarkImportant = () => {
-            this.setState(({important}) => { /*деструктурируем из state свойство important*/
-                return {
-                    important: !important
-                }
-            });
-        };
+    // при клике на кнопку добавляем/убираем к классу класс 'important'
+    // выделяем "дело" жирным шрифтом
+    this.onMarkImportant = () => {
+      this.setState(({important}) => { /*деструктурируем из state свойство important*/
+        return {
+          important: !important
+        }
+      });
+    };
+  }
+
+  render() {
+    const {
+      label, onDeleted, done,
+      onToggleDone
+    } = this.props;
+    const {important} = this.state;
+
+    let classNames = 'todo-list-item';
+
+    // если true, то добавляем класс done
+    if (done) {
+      classNames += ' done';
+    }
+    // если true, то добавляем класс important
+    if (important) {
+      classNames += ' important'
     }
 
-    render() {
-        const { label, onDeleted, done,
-                onToggleDone } = this.props;
-        const { important } = this.state;
-
-        let classNames = 'todo-list-item';
-
-        // если true, то добавляем класс done
-        if (done) {
-            classNames += ' done';
-        }
-        // если true, то добавляем класс important
-        if (important) {
-            classNames += ' important'
-        }
-
-        return (
-            <span className={ classNames }>
+    return (
+      <span className={classNames}>
                 <span
-                    className="todo-list-item-label"
-                    onClick = { onToggleDone }>
+                  className="todo-list-item-label"
+                  onClick={onToggleDone}>
                     {label}
                 </span>
 
                 <button type="button"
                         className="btn btn-outline-success btn-sm float-right"
-                        onClick = { this.onMarkImportant }>
+                        onClick={this.onMarkImportant}>
                 <i className="fa fa-exclamation"/>
                 </button>
 
                 <button type="button"
                         className="btn btn-outline-danger btn-sm float-right"
-                        /* вызываем при клике функцию родительском элементе */
-                        onClick = { onDeleted } >
+                  /* вызываем при клике функцию родительском элементе */
+                        onClick={onDeleted}>
                 <i className="fa fa-trash-o"/>
                 </button>
             </span>
-        );
-    };
+    );
+  };
 }
--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -3,30 +3,30 @@ import './todo-list.css';
 
 import TodoListItem from '../todo-list-item';
 
-const TodoList = ({ todos, onDeleted, onToggleDone }) => {
+const TodoList = ({todos, onDeleted, onToggleDone}) => {
 
-    const elements = todos.map((item) => {
+  const elements = todos.map((item) => {
 
-        // выделяем id из массива объектов и передаем его отдельно
-        // в ...itemProps передаем все значения с одинаковыми названиями кроме id
-        const { id, ...itemProps } = item;
-
-        return (
-            <li key={id} className="list-group-item">
-                <TodoListItem
-                    { ...itemProps }
-                    onDeleted = {() => onDeleted(id) }
-                    onToggleDone = {() => onToggleDone(id)}/>
-            </li>
-        );
-    });
-
+    // выделяем id из массива объектов и передаем его отдельно
+    // в ...itemProps передаем все значения с одинаковыми названиями кроме id
+    const {id, ...itemProps} = item;
 
     return (
-        <ul className="list-group todo-list">
-            { elements }
-        </ul>
+      <li key={id} className="list-group-item">
+        <TodoListItem
+          {...itemProps}
+          onDeleted={() => onDeleted(id)}
+          onToggleDone={() => onToggleDone(id)}/>
+      </li>
     );
+  });
+
+
+  return (
+    <ul className="list-group todo-list">
+      {elements}
+    </ul>
+  );
 };
 
 export default TodoList;
